9920477c192a03591747e422e766e60b0d7c8f75
Sebibebi67 <sebibebi67@gmail.com>
2020-04-10 10:21:45 +0200

    changing reading method

--- src/exec.py
+++ src/exec.py
@@ -7,25 +7,38 @@ import sys
 #
 
 pile = []
-cptLigne = 1
-cptSaut = 0
+cptLigne = 0
+# cptSaut = 0
+
+fin = False
+
+programme=[]
 
 
 def debutProg():
     print("Debut de Programme\n")
+    global cptLigne
+    cptLigne += 1
 
 
 def finProg():
     print("\nFin de Programme")
+    global fin
+    fin = True
 
 
 def reserver(n):
+    global cptLigne
+    cpt = cptLigne
     for i in range(0, n):
         empiler(0)
+    cptLigne = cpt+1
 
 
 def empiler(n):
     pile.append(n)
+    global cptLigne
+    cptLigne += 1
 
 
 def depiler():
@@ -38,17 +51,24 @@ def affectation():
     a = depiler()
     global pile
     pile[a] = n
+    global cptLigne
+    cptLigne += 1
 
 
 def valeurPile():
     # cherche la valeur a situee a l'adresse n
+    global cptLigne
+    cpt = cptLigne
     n = depiler()
     a = pile[n]
     empiler(a)
+    cptLigne = cpt+1
 
 
 def get():
     # recupere l'element saisi au clavier
+    global cptLigne
+    cpt = cptLigne
     ok = False
     while not ok:
         a = input("Entrez un nombre a empiler : ")
@@ -60,121 +80,165 @@ def get():
             ok = True
         except ValueError:
             print("Rentrez une valeur entiere")
-
+    cptLigne = cpt+1
 
 def put():
     a = depiler()
     print(a)
+    global cptLigne
+    cptLigne += 1
 
 
 def moins():
+    global cptLigne
+    cpt = cptLigne
     a = depiler()
     empiler(-a)
+    cptLigne = cpt+1
 
 
 def sous():
     # a - b
+    global cptLigne
+    cpt = cptLigne
     b = depiler()
     a = depiler()
     empiler(a-b)
+    cptLigne = cpt+1
 
 
 def add():
     # a + b
+    global cptLigne
+    cpt = cptLigne
     b = depiler()
     a = depiler()
     empiler(a+b)
+    cptLigne = cpt+1
 
 
 def mult():
     # a + b
+    global cptLigne
+    cpt = cptLigne
     b = depiler()
     a = depiler()
     empiler(a*b)
+    cptLigne = cpt+1
 
 
 def div():
     # a//b
+    global cptLigne
+    cpt = cptLigne
     b = depiler()
     a = depiler()
     empiler(a//b)
+    cptLigne = cpt+1
 
 
 def egal():
     # a == b
+    global cptLigne
+    cpt = cptLigne
     b = depiler()
     a = depiler()
     empiler(int(a == b))
+    cptLigne = cpt+1
 
 
 def diff():
     # a != b
+    global cptLigne
+    cpt = cptLigne
     b = depiler()
     a = depiler()
     empiler(int(a != b))
+    cptLigne = cpt+1
 
 
 def inf():
     # a < b
+    global cptLigne
+    cpt = cptLigne
     b = depiler()
     a = depiler()
     empiler(int(a < b))
+    cptLigne = cpt+1
 
 
 def infeg():
     # a <= b
+    global cptLigne
+    cpt = cptLigne
     b = depiler()
     a = depiler()
     empiler(int(a <= b))
+    cptLigne = cpt+1
 
 
 def sup():
     # a > b
+    global cptLigne
+    cpt = cptLigne
     b = depiler()
     a = depiler()
     empiler(int(a > b))
+    cptLigne = cpt+1
 
 
 def supeg():
     # a >= b
+    global cptLigne
+    cpt = cptLigne
     b = depiler()
     a = depiler()
     empiler(int(a >= b))
+    cptLigne = cpt+1
 
 
 def et():
     # bool1 et bool2
+    global cptLigne
+    cpt = cptLigne
     bool1 = depiler()
     bool2 = depiler()
     empiler(int(bool1 and bool2))
+    cptLigne = cpt+1
 
 
 def ou():
     # bool1 ou bool2
+    global cptLigne
+    cpt = cptLigne
     bool1 = depiler()
     bool2 = depiler()
     empiler(int(bool1 or bool2))
+    cptLigne = cpt+1
 
 
 def non():
     # non boolean
+    global cptLigne
+    cpt = cptLigne
     boolean = depiler()
     empiler(int(not boolean))
+    cptLigne = cpt+1
 
 
 def tra(n):
-    # print("tra")
-    # print(n)
-    global cptSaut 
-    cptSaut = n
+    global cptLigne 
+    cptLigne = n-1
 
 
 def tze(n):
     # print("tze")
     boolean = depiler()
+    global cptLigne 
     if not bool(boolean):
-        global cptSaut 
-        cptSaut = n
+        cptLigne = n-1
+    else:
+        cptLigne +=1 
 
 
 def erreur(exp):
@@ -186,10 +250,7 @@ def erreur(exp):
 
 with open("testFiles/prog2.txt") as f:
     for line in f:
-        # print(cptLigne)
-        # print(cptSaut)
-        if cptSaut <= cptLigne:
-            eval(line.split(";")[0])
-            print(cptLigne, pile)
-        cptLigne += 1
-        # print(pile)
+        programme.append(line.split(";")[0])
+    while not fin:
+        print(cptLigne+1)
+        eval(programme[cptLigne])
