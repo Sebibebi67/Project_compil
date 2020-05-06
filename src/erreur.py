7abc720d0950b86a63166f3bb09d57ad80878f04
Sebibebi67 <sebibebi67@gmail.com>
2020-05-06 16:26:23 +0200

    new file

---
+++ src/erreur.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+#======================exec.py=====================#
+
+
+#---------------------Encodage---------------------#
+
+# -*- coding: utf-8 -*-
+
+#--------------------------------------------------#
+
+
+#---------------------Imports----------------------#
+
+import sys
+
+#--------------------------------------------------#
+
+
+#--------------------Description-------------------#
+
+# Ce fichier gère les potentielles erreurs de code
+#donné en paramètre du programme, permettant le bon
+#fonctionnement de ce dernier
+
+#--------------------------------------------------#
+
+
+#----------------------Auteurs---------------------#
+
+# Sébastien HERT
+# Adam RIVIERE
+
+#--------------------------------------------------#
+
+
+#----------------Variables globales----------------#
+
+
+
+#--------------------------------------------------#
+
+
+#---------------------Méthodes---------------------#
+
+def checkErreurs():
+    return True
+
+
+# lecture d'un booléen
+# écriture d'expressions booléennes
+# typage incorrect
+# condition incorrecte dans une alternative
+# condition incorrecte dans un while
+# double déclaration
