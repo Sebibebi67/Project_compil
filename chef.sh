5cfbe6e404ab74457055a8490af54ecb3aba8627
Sebibebi67 <sebibebi67@gmail.com>
2020-05-20 16:31:57 +0200

    adding utf-8 encoding remplacing "?" by correct char in test files

--- chef.sh
+++ chef.sh
@@ -7,9 +7,9 @@
 rm -r tmp
 mkdir tmp
 
-python src/anasyn_Table.py $1 --show-ident-table >> tmp/IdentTable.txt
+./src/anasyn_Table.py $1 --show-ident-table >> tmp/IdentTable.txt
 
-ident_list=$( python src/anasyn_Table.py $1 --show-ident-list )
+ident_list=$( ./src/anasyn_Table.py $1 --show-ident-list )
 # echo $ident_list
 
 ./src/gencode.py $ident_list >> tmp/NilNovi.txt
