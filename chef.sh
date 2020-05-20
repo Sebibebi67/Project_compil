63884c286a900baccd92cf89ed1c1aa524ed147d
Sebibebi67 <sebibebi67@gmail.com>
2020-05-20 15:45:28 +0200

    update gitignore doing global shell script

---
+++ chef.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#compilateur heuristique efficace et fiable
+
+# echo $1
+
+rm -r tmp
+mkdir tmp
+
+python src/anasyn_Table.py $1 --show-ident-table >> tmp/IdentTable.txt
+
+ident_list=$( python src/anasyn_Table.py $1 --show-ident-list )
+echo $ident_list
+
+./src/gencode.py $ident_list >> tmp/NilNovi.txt
