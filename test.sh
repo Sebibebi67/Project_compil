a08b4765c5bb5d80115e6b75d29293c60832bf67
Sebibebi67 <sebibebi67@gmail.com>
2020-05-29 11:11:37 +0200

    updating errors, upgrading test.sh

--- test.sh
+++ test.sh
@@ -1,13 +1,102 @@
 #!/bin/bash
 
+
+#=================================== test.sh ====================================#
+
+
+#--------------------------------- Description ----------------------------------#
+#
+# Test l'intégralité des fichiers de test/nna et test/nnp
+#
+#--------------------------------------------------------------------------------#
+
+
+#----------------------------------- Synopsis -----------------------------------#
+#
+# ./test.sh 
+#
+#--------------------------------------------------------------------------------#
+
+
+#----------------------------------- Auteurs ------------------------------------#
+#
+# Sébastien HERT
+#
+#--------------------------------------------------------------------------------#
+
+
+#------------------------------ Variables Globales ------------------------------#
+
+lenTitle=80
+
+#--------------------------------------------------------------------------------#
+
+
+#---------------------------------- Fonctions -----------------------------------#
+
+function titre(){
+    ###
+    # Description : Affiche un titre dans le terminal
+    #
+    # Input :
+    # - l'indentifieur pour compléter la ligne
+    # - Le titre sous la forme d'une chaine de caractère
+    #
+    # Output :
+    # - Affichage dans le terminal
+    #
+    # Auteur :
+    # - Sébastien HERT
+    ###
+
+    if [ $# -eq 0 ]; then
+        param="Titre"
+        id="="
+    elif [ $# -eq 1 ]; then
+        param=$1
+        id="*"
+    elif [ $# -eq 2 ]; then
+        param=$2
+        id=$1
+    else
+        echo error
+    fi
+
+    lenParam=${#param}
+    lenEq1=$(( ($lenTitle-$lenParam)/2 -1 ))
+    lenEq2=$(( $lenTitle-$lenParam-$lenEq1-2 ))
+    title=""
+    for (( i = 0; i < $lenEq1; i++ )); do
+        title="${title}$1"
+    done
+    title="${title} $param "
+    for (( i = 0; i < $lenEq2; i++ )); do
+        title="${title}$1"
+    done
+
+    echo -e "\e[93m\e[1m${title}\n\e[0m"
+}
+
+#--------------------------------------------------------------------------------#
+
+
+#------------------------------------- Main -------------------------------------#
+
 set -e
 
 paths="./test/nna ./test/nnp"
 
+
 for path in $paths; do
 	for file in $(ls $path) ; do
-		echo -e "\e[93mTesting $path/$file\e[0m\n"
+		titre '*' "Testing ${path}/${file}"
 		./chef.sh -c $path/$file
-		echo -e "\n"
 	done
-done	
+done
+
+#--------------------------------------------------------------------------------#
+
+
+#================================================================================#
+
+	
